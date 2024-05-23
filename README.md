ab2b599cf483d580878de86405a656a2700e03f7
Mena WANG <ningwang25@gmail.com>
2024-05-23 22:57:39 +1000

    multistep agent and granular control

--- README.md
+++ README.md
@@ -1,15 +1,25 @@
 # Agentic-RAG-with-Llamaindex
 
-This is my code and studynote for the course [Building Agentic RAG with LlamaIndex](https://www.deeplearning.ai/short-courses/building-agentic-rag-with-llamaindex/) 📚
+This is my code and studynote for the course [Building Agentic RAG with LlamaIndex](https://www.deeplearning.ai/short-courses/building-agentic-rag-with-llamaindex/) 📚 A super informative and practical course. Highly recommended! 😎 
 
-# Lesson 1. Tools Calling with a Router Engine
+# ✅ Lesson 1. Tools Calling using a Router Engine
 
 * In standard RAG, LLM are mainlysed used forr synthesis of information only.
-* A router engine adds additional functionality which enable LLM to determine whether to implement a Q&A or a summarization query engine/tool based on the question from the user
-* See `L1_Router_Engine` notebook & `get_router_query_engine()` in `utils`
+* A router engine adds additional functionality which enables LLM to determine whether to implement a Q&A or a summarization query engine/tool based on the question from the user
+* See `L1_Router_Engine` notebook & `get_router_query_engine()` function in `utils`
 
-# Lesson 2. Tool Calling & Infer Parameters
+# ✅ Lesson 2. Tool Calling & Infer Parameters
 
 * In this interation, the LLM not only helps choosing the appropriate tool but also infer necessary arguments for execution. 
 * The end result is that users get to ask more questions and receive more preciese results. 
+* See `L2_Tool_Calling`notebook & `get_doc_tools()` function in `utils`
 
+# ✅ Lesson 3. Multi-steps Agent
+
+* Use AgentRunner to plan and orchestrate multi-step tasks 
+* Use AgentWorker to execute the tasks. 
+* Human users can 
+    * inspect and chat with the agent about its reasoning and responses 💭
+    * control the agent in a granular fashion, which also enhances debuggability and steerability of the agent. 🧭
+
+![Agent](image\agentRunner-and-agentWorker.PNG)
--- utils.py
+++ utils.py
@@ -1,12 +1,14 @@
-from llama_index.core import SimpleDirectoryReader
+from llama_index.core import SimpleDirectoryReader, VectorStoreIndex, SummaryIndex
 from llama_index.core.node_parser import SentenceSplitter
 from llama_index.core import Settings
 from llama_index.llms.openai import OpenAI
 from llama_index.embeddings.openai import OpenAIEmbedding
 from llama_index.core import SummaryIndex, VectorStoreIndex
-from llama_index.core.tools import QueryEngineTool
+from llama_index.core.tools import QueryEngineTool, FunctionTool
 from llama_index.core.query_engine.router_query_engine import RouterQueryEngine
 from llama_index.core.selectors import LLMSingleSelector
+from llama_index.core.vector_stores import MetadataFilters, FilterCondition
+from typing import List, Optional
 
 
 def get_router_query_engine(file_path: str, llm = None, embed_model = None):
@@ -53,3 +55,70 @@ def get_router_query_engine(file_path: str, llm = None, embed_model = None):
         verbose=True
     )
     return query_engine
+
+
+def get_doc_tools(
+    file_path: str,
+    name: str,
+) -> str:
+    """Get vector query and summary query tools from a document."""
+
+    # load documents
+    documents = SimpleDirectoryReader(input_files=[file_path]).load_data()
+    splitter = SentenceSplitter(chunk_size=1024)
+    nodes = splitter.get_nodes_from_documents(documents)
+    vector_index = VectorStoreIndex(nodes)
+    
+    def vector_query(
+        query: str, 
+        page_numbers: Optional[List[str]] = None
+    ) -> str:
+        """Use to answer questions over the MetaGPT paper.
+    
+        Useful if you have specific questions over the MetaGPT paper.
+        Always leave page_numbers as None UNLESS there is a specific page you want to search for.
+    
+        Args:
+            query (str): the string query to be embedded.
+            page_numbers (Optional[List[str]]): Filter by set of pages. Leave as NONE 
+                if we want to perform a vector search
+                over all pages. Otherwise, filter by the set of specified pages.
+        
+        """
+    
+        page_numbers = page_numbers or []
+        metadata_dicts = [
+            {"key": "page_label", "value": p} for p in page_numbers
+        ]
+        
+        query_engine = vector_index.as_query_engine(
+            similarity_top_k=2,
+            filters=MetadataFilters.from_dicts(
+                metadata_dicts,
+                condition=FilterCondition.OR
+            )
+        )
+        response = query_engine.query(query)
+        return response
+        
+    
+    vector_query_tool = FunctionTool.from_defaults(
+        name=f"vector_tool_{name}",
+        fn=vector_query
+    )
+    
+    summary_index = SummaryIndex(nodes)
+    summary_query_engine = summary_index.as_query_engine(
+        response_mode="tree_summarize",
+        use_async=True,
+    )
+    summary_tool = QueryEngineTool.from_defaults(
+        name=f"summary_tool_{name}",
+        query_engine=summary_query_engine,
+        description=(
+            "Use ONLY IF you want to get a holistic summary of MetaGPT. "
+            "Do NOT use if you have specific questions over MetaGPT."
+        ),
+    )
+
+    return vector_query_tool, summary_tool
